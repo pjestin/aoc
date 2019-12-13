149ee6c6002ac51f618882cf2082151cabee57b1
Pierre Jestin <pierre.jestin@intercom.io>
2019-12-13 15:12:31 +0000

    Complete puzzle with IA

--- 13/index.js
+++ 13/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs'),
     path = require('path'),
-    readline = require('readline-sync'),
     runIntcode = require('../lib/intcode'),
     INPUT_FILE_PATH = path.join(__dirname, 'intcode-input.txt'),
     INITIAL_INTCODE = fs.readFileSync(INPUT_FILE_PATH, 'utf8').trim().split(',').map(Number);
@@ -36,10 +35,10 @@ const countBlockTiles = (tiles) => {
 const displayGame = (gameData) => {
     const charMap = {
         0: ' ',
-        1: 'X',
-        2: '#',
-        3: '_',
-        4: '*'
+        1: '█',
+        2: '░',
+        3: '—',
+        4: 'o'
     }
     const gameDisplay = gameData.tiles.map(row => row.reduce((rowDisplay, tile) => rowDisplay + charMap[tile], ''))
         .reduce((display, curRow) => display + curRow + '\n', '');
@@ -47,46 +46,62 @@ const displayGame = (gameData) => {
     console.log(`Score: ${gameData.score}`);
 }
 
-const getJoystickInput = () => {
-    const input = readline.question("Joystick input? [a,s,d] ");
-    switch (input) {
-        case 'a':
-            return -1;
-        case 'd':
-            return 1;
-        default:
-            return 0;
+const getJoystickInput = (tiles) => {
+    let ballX = 0;
+    let paddleX = 0;
+    for (let y = 0; y < tiles.length; ++y) {
+        for (let x = 0; x < tiles[0].length; ++x) {
+            if (tiles[y][x] === 3) {
+                paddleX = x;
+            } else if (tiles[y][x] === 4) {
+                ballX = x;
+            }
+        }
+    }
+    if (ballX < paddleX) {
+        return -1;
+    } else if (ballX > paddleX) {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
 const playGame = () => {
-    let computer = {
+    let computers = [{
         memory: Object.assign({}, INITIAL_INTCODE),
         input: [],
         index: 0,
         relativeBase: 0
-    }
-    computer.memory[0] = 2;
+    }];
+    computers[0].memory[0] = 2;
     let blockCount = 1;
     let gameData = {};
     while (blockCount !== 0) {
         let tiles = [];
+        let currentComputer = JSON.parse(JSON.stringify(computers[computers.length - 1]));
         while (true) {
-            computer = runIntcode(computer.memory, computer.input, computer.index, computer.relativeBase);
-            if (computer === null) {
-                console.log('You lost!');
+            currentComputer = runIntcode(currentComputer.memory, currentComputer.input, currentComputer.index, currentComputer.relativeBase);
+            if (currentComputer === null) {
+                updateGameData(gameData, tiles);
+                if (countBlockTiles(gameData.tiles) === 0) {
+                    console.log(`You won! Score: ${gameData.score}`);
+                } else {
+                    console.log(`You lost! Score: ${gameData.score}`);
+                }
                 return;
-            } else if (computer.needInput) {
+            } else if (currentComputer.needInput) {
                 break;
             }
-            tiles.push(computer.output);
+            tiles.push(currentComputer.output);
         }
         updateGameData(gameData, tiles);
         displayGame(gameData);
         blockCount = countBlockTiles(gameData.tiles);
-        computer.input.push(getJoystickInput());
+        const input = getJoystickInput(gameData.tiles);
+        currentComputer.input.push(input);
+        computers.push(currentComputer);
     }
-    console.log(`You won! Score: ${gameData.score}`);
 }
 
 playGame();
