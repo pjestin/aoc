82869e5302c1a1ddef80c922bf8c58c2ccba0bd3
Pierre Jestin <pierre.jestin@gmail.com>
2020-11-28 15:24:01 +0100

    First commit

---
+++ src/main/java/day01/Day01.java
@@ -0,0 +1,7 @@
+package day01;
+
+public class Day01 {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+}
---
+++ src/test/java/day01/Day01Test.java
@@ -0,0 +1,11 @@
+package day01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class Day01Test {
+  @Test
+  public void part1Test() {
+    assertEquals(3, Day01.sum(1, 2));
+  }
+}
