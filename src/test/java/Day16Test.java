b59f0ee8d5cbff74c42060cac2e019e597c25e6f
Pierre Jestin <pierre.jestin@gmail.com>
2020-12-13 20:31:11 +0100

    Day 16, part 2 ongoing

---
+++ src/test/java/Day16Test.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Day16Test {
+  private static List<String> inputTestLines, inputLines;
+
+  @BeforeAll
+  public static void setUp() throws FileNotFoundException {
+    inputTestLines = FileUtils.readLines(Paths.get("src", "test", "resources", "day16", "input-test.txt"));
+    inputLines = FileUtils.readLines(Paths.get("src", "test", "resources", "day16", "input.txt"));
+  }
+
+  @Test
+  public void countMultimorphicSamplesTest() {
+    assertEquals(1, Day16.countMultimorphicSamples(inputTestLines));
+    assertEquals(567, Day16.countMultimorphicSamples(inputLines));
+  }
+
+  @Test
+  public void runInputProgramTest() throws Exception {
+    assertEquals(567, Day16.runInputProgram(inputLines));
+  }
+}
