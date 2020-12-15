c20ac1a1424eb5521c8db6adef18553c0be5ba21
Pierre Jestin <pierre.jestin@gmail.com>
2020-12-15 13:34:18 +0100

    Day 15 optimization

--- day15/day15.go
+++ day15/day15.go
@@ -1,20 +1,20 @@
 package day15
 
-func GetMemoryGameNthNumber(startingNumbers []uint64, n uint64) uint64 {
-	memory := make(map[uint64]uint64)
-	turn := uint64(1)
+func GetMemoryGameNthNumber(startingNumbers []uint32, n uint32) uint32 {
+	memory := make([]uint32, n+1)
+	turn := uint32(1)
 	for _, startingNumber := range startingNumbers[:len(startingNumbers)-1] {
 		memory[startingNumber] = turn
 		turn++
 	}
 	currentNumber := startingNumbers[len(startingNumbers)-1]
 	for turn < n {
-		previousTurn, present := memory[currentNumber]
-		var nextNumber uint64
-		if present {
-			nextNumber = turn - previousTurn
-		} else {
+		previousTurn := memory[currentNumber]
+		var nextNumber uint32
+		if previousTurn == 0 {
 			nextNumber = 0
+		} else {
+			nextNumber = turn - previousTurn
 		}
 		memory[currentNumber] = turn
 		currentNumber = nextNumber
--- day15/day15_test.go
+++ day15/day15_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func TestGetMemoryGameNthNumber2020(t *testing.T) {
-	res := GetMemoryGameNthNumber([]uint64{0, 3, 6}, 2020)
-	require.Equal(t, uint64(436), res)
-	res = GetMemoryGameNthNumber([]uint64{19, 20, 14, 0, 9, 1}, 2020)
-	require.Equal(t, uint64(1325), res)
+	res := GetMemoryGameNthNumber([]uint32{0, 3, 6}, 2020)
+	require.Equal(t, uint32(436), res)
+	res = GetMemoryGameNthNumber([]uint32{19, 20, 14, 0, 9, 1}, 2020)
+	require.Equal(t, uint32(1325), res)
 }
 
 func TestGetMemoryGameNthNumber30000000(t *testing.T) {
-	res := GetMemoryGameNthNumber([]uint64{0, 3, 6}, 30000000)
-	require.Equal(t, uint64(175594), res)
-	res = GetMemoryGameNthNumber([]uint64{19, 20, 14, 0, 9, 1}, 30000000)
-	require.Equal(t, uint64(59006), res)
+	res := GetMemoryGameNthNumber([]uint32{0, 3, 6}, 30000000)
+	require.Equal(t, uint32(175594), res)
+	res = GetMemoryGameNthNumber([]uint32{19, 20, 14, 0, 9, 1}, 30000000)
+	require.Equal(t, uint32(59006), res)
 }
