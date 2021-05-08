3e72bd2ef77e575c69a0253561d7a1500812350f
Pierre Jestin <pierre.jestin@gmail.com>
2021-05-08 19:28:48 +0200

    ANother attempt at day 15

--- src/main/java/com/pjestin/aoc2018/Day15.java
+++ src/main/java/com/pjestin/aoc2018/Day15.java
@@ -1,195 +1,269 @@
 package com.pjestin.aoc2018;
 
-import java.util.List;
-import java.util.OptionalInt;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.TreeMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.stream.Collectors;
+
 import com.pjestin.lib.Vector;
 
 public class Day15 {
-  private static class Unit {
-    public Vector position;
-    public int hp = 200;
-    public boolean isElf;
+  private static final char WALL_CHARACTER = '#';
+  private static final char ELF_CHARACTER = 'E';
+  private static final char GOBLIN_CHARACTER = 'G';
 
+  public static final Vector[] RANGE = { new Vector(0, -1), new Vector(-1, 0), new Vector(1, 0), new Vector(0, 1) };
+
+  private static class Unit {
     public static final int ATTACK_POINTS = 3;
 
-    public static final Vector[] RANGE = {new Vector(0, -1), new Vector(-1, 0), new Vector(1, 0), new Vector(0, 1)};
+    public boolean isElf;
+    public Vector position;
+    public int hp = 200;
 
-    public Unit(Vector position, boolean isElf) {
-      this.position = position;
+    public Unit(boolean isElf, Vector position) {
       this.isElf = isElf;
+      this.position = position;
     }
   }
 
-  private static Set<Vector> parseWalls(List<String> lines) {
-    Set<Vector> walls = new HashSet<>();
-    for (int y = 0; y < lines.size(); y++) {
-      String line = lines.get(y);
-      for (int x = 0; x < line.length(); x++) {
-        if (line.charAt(x) == '#') {
-          walls.add(new Vector(x, y));
+  private static class Battle {
+    Set<Vector> walls;
+    Map<Vector, Unit> units;
+
+    public Battle(Set<Vector> walls, Map<Vector, Unit> units) {
+      this.walls = walls;
+      this.units = units;
+    }
+
+    public String toString() {
+      Vector maxPosition = new Vector(0, 0);
+      for (Vector wall : this.walls) {
+        if (wall.x > maxPosition.x) {
+          maxPosition.x = wall.x;
+        }
+        if (wall.y > maxPosition.y) {
+          maxPosition.y = wall.y;
         }
       }
-    }
-    return walls;
-  }
-
-  private static TreeMap<Vector, Unit> parseUnits(List<String> lines) {
-    TreeMap<Vector, Unit> units = new TreeMap<>();
-    for (int y = 0; y < lines.size(); y++) {
-      String line = lines.get(y);
-      for (int x = 0; x < line.length(); x++) {
-        if (line.charAt(x) == 'E' || line.charAt(x) == 'G') {
+      StringBuilder sb = new StringBuilder();
+      for (int y = 0; y <= maxPosition.y; ++y) {
+        for (int x = 0; x <= maxPosition.x; ++x) {
           Vector position = new Vector(x, y);
-          units.put(position, new Unit(position, line.charAt(x) == 'E'));
+          if (this.walls.contains(position)) {
+            sb.append('#');
+          } else if (this.units.containsKey(position)) {
+            sb.append(this.units.get(position).isElf ? 'E' : 'G');
+          } else {
+            sb.append('.');
+          }
         }
+        sb.append('\n');
       }
+      return sb.toString();
     }
-    return units;
-  }
 
-  private static Unit getUnitInRange(Unit unit, TreeMap<Vector, Unit> units) {
-    List<Unit> unitsInRange = new ArrayList<>();
-    for (Vector direction : Unit.RANGE) {
-      Vector rangePosition = new Vector(unit.position).add(direction);
-      if (units.containsKey(rangePosition)) {
-        Unit unitInRange = units.get(rangePosition);
-        if (unitInRange.isElf != unit.isElf) {
-          unitsInRange.add(unitInRange);
+    private Set<Vector> findTargetRanges(Unit unit) {
+      // System.out.println("findTargetRanges");
+      Set<Vector> targetRanges = new HashSet<>();
+      List<Unit> targets = this.units.values().stream().filter(target -> target.isElf != unit.isElf)
+          .collect(Collectors.toList());
+      for (Unit target : targets) {
+        for (Vector direction : RANGE) {
+          Vector positionInRange = new Vector(target.position).add(direction);
+          if (!this.units.containsKey(positionInRange) && !this.walls.contains(positionInRange)) {
+            targetRanges.add(positionInRange);
+          }
         }
       }
+      return targetRanges;
     }
-    if (unitsInRange.isEmpty()) {
+
+    private class QueueState implements Comparable<QueueState> {
+      List<Vector> path;
+
+      public QueueState(List<Vector> path) {
+        this.path = path;
+      }
+
+      @Override
+      public int compareTo(QueueState other) {
+        if (this.path.size() == other.path.size()) {
+          int lastPathCompare = this.path.get(this.path.size() - 1).compareTo(other.path.get(other.path.size() - 1));
+          if (lastPathCompare == 0) {
+            return this.path.get(0).compareTo(other.path.get(0));
+          }
+          return lastPathCompare;
+        }
+        return this.path.size() - other.path.size();
+      }
+    }
+
+    private Vector findNextStep(Unit unit, Set<Vector> targetRanges) {
+      // System.out.println("findNextStep");
+      PriorityQueue<QueueState> queue = new PriorityQueue<>();
+      queue.add(new QueueState(Arrays.asList(unit.position)));
+      Set<Vector> visited = new HashSet<>();
+      while (!queue.isEmpty()) {
+        QueueState state = queue.remove();
+        Vector position = state.path.get(state.path.size() - 1);
+        if (this.walls.contains(position) || (this.units.containsKey(position) && !unit.position.equals(position))
+            || visited.contains(position)) {
+          continue;
+        }
+        visited.add(position);
+        if (targetRanges.contains(position)) {
+          // Found shortest path, returning 2nd path position, which is next step
+          return state.path.get(1);
+        }
+        for (Vector direction : RANGE) {
+          List<Vector> pathCopy = state.path.stream().collect(Collectors.toList());
+          Vector nextPosition = new Vector(position).add(direction);
+          pathCopy.add(nextPosition);
+          queue.add(new QueueState(pathCopy));
+        }
+      }
       return null;
     }
-    Unit minHpUnit = null;
-    int minHp = Integer.MAX_VALUE;
-    for (Unit unitInRange : unitsInRange) {
-      if (unitInRange.hp < minHp) {
-        minHp = unitInRange.hp;
-        minHpUnit = unitInRange;
+
+    private void move(Unit unit) {
+      // System.out.println("move");
+      for (Vector direction : RANGE) {
+        Vector target = new Vector(unit.position).add(direction);
+        if (this.units.containsKey(target) && this.units.get(target).isElf != unit.isElf) {
+          // Unit is next to enemy target, no need to move
+          return;
+        }
+      }
+      Set<Vector> targetRanges = this.findTargetRanges(unit);
+      // System.out.println(String.format("Unit at position %s; target ranges: %s",
+      // unit.position, targetRanges));
+      if (targetRanges.isEmpty()) {
+        // No enemy has accessible square in range
+        return;
+      }
+      Vector nextStep = this.findNextStep(unit, targetRanges);
+      if (nextStep == null) {
+        // No path to target ranges
+        return;
+      }
+      this.units.remove(unit.position);
+      unit.position = nextStep;
+      this.units.put(nextStep, unit);
+    }
+
+    public void attack(Unit unit) {
+      // System.out.println("attack");
+      // System.out.println(String.format("Unit position: %s", unit.position));
+      List<Unit> unitsInRange = new ArrayList<>();
+      for (Vector direction : RANGE) {
+        Vector rangePosition = new Vector(unit.position).add(direction);
+        if (this.units.containsKey(rangePosition)) {
+          Unit rangeUnit = this.units.get(rangePosition);
+          if (rangeUnit.isElf != unit.isElf) {
+            unitsInRange.add(rangeUnit);
+          }
+        }
+      }
+      if (unitsInRange.isEmpty()) {
+        return;
+      }
+      unitsInRange.sort((unit1, unit2) -> {
+        if (unit1.hp == unit2.hp) {
+          return unit1.position.compareTo(unit2.position);
+        }
+        return unit1.hp - unit2.hp;
+      });
+      Unit target = unitsInRange.get(0);
+      target.hp -= Unit.ATTACK_POINTS;
+      if (target.hp <= 0) {
+        this.units.remove(target.position);
       }
     }
-    return minHpUnit;
-  }
 
-  private static class State {
-    public Vector position;
-    public LinkedList<Vector> path = new LinkedList<>();
+    public boolean playTurn() {
+      // System.out.println("playTurn");
+      List<Unit> unitList = this.units.values().stream().collect(Collectors.toList());
+      unitList.sort((unit1, unit2) -> unit1.position.compareTo(unit2.position));
+      for (Unit unit : unitList) {
+        // System.out.println(String.format("Unit position: %s", unit.position));
+        if (unit.hp <= 0) {
+          continue;
+        }
 
-    public State(Vector position) {
-      this.position = position;
-    }
+        if (this.units.values().stream().filter(target -> target.isElf != unit.isElf).count() == 0) {
+          // No targets left
+          return false;
+        }
 
-    public State(State other) {
-      this.position = other.position;
-      this.path = (LinkedList<Vector>)other.path.clone();
-    }
-  }
+        // Move phase
+        this.move(unit);
 
-  private static Set<Vector> getEnemyRanges(boolean shouldBeElf, TreeMap<Vector, Unit> units, Set<Vector> walls) {
-    Set<Vector> enemyRanges = new HashSet<>();
-    for (Unit potentialEnemy : units.values()) {
-      if (potentialEnemy.isElf != shouldBeElf) {
-        continue;
+        // Attack phase
+        this.attack(unit);
       }
-      for (Vector direction : Unit.RANGE) {
-        Vector position = new Vector(potentialEnemy.position).add(direction);
-        if (!walls.contains(position) && !units.containsKey(position)) {
-          enemyRanges.add(position);
+      return true;
+    }
+
+    public int outcome() {
+      return this.units.values().stream().map(unit -> unit.hp).reduce(0, Integer::sum);
+    }
+  }
+
+  public static Battle parseWallsAndUnits(List<String> lines) {
+    // System.out.println("parseWallsAndUnits");
+    Set<Vector> walls = new HashSet<>();
+    Map<Vector, Unit> units = new HashMap<>();
+    for (int y = 0; y < lines.size(); ++y) {
+      String line = lines.get(y);
+      for (int x = 0; x < line.length(); ++x) {
+        char character = line.charAt(x);
+        switch (character) {
+          case WALL_CHARACTER:
+            walls.add(new Vector(x, y));
+            break;
+          case ELF_CHARACTER:
+          case GOBLIN_CHARACTER:
+            Vector position = new Vector(x, y);
+            units.put(position, new Unit(character == ELF_CHARACTER, position));
+            break;
         }
       }
     }
-    return enemyRanges;
-  }
-
-  private static void moveUnit(Unit unit, TreeMap<Vector, Unit> units, Set<Vector> walls) {
-    Unit potentialUnitInRange = getUnitInRange(unit, units);
-    if (potentialUnitInRange != null) {
-      return;
-    }
-
-    // System.out.println(String.format("Moving unit at %s", unit.position));
-    Set<Vector> enemyRanges = getEnemyRanges(!unit.isElf, units, walls);
-    Set<Vector> visited = new HashSet<>();
-    LinkedList<State> queue = new LinkedList<>();
-    queue.add(new State(unit.position));
-    while (!queue.isEmpty()) {
-      State state = queue.pop();
-      Vector position = state.position;
-      if (walls.contains(position) || visited.contains(position)) {
-        continue;
-      }
-      // System.out.println(String.format("At %s", position));
-      visited.add(position);
-      if (enemyRanges.contains(position)) {
-        // System.out.println(String.format("Found enemy range at %s, moving to %s", position, state.path.peek()));
-        units.remove(unit.position);
-        unit.position = state.path.peek();
-        units.put(unit.position, unit);
-        return;
-      } else if (!position.equals(unit.position) && units.containsKey(position)) {
-        continue;
-      }
-      for (Vector direction : Unit.RANGE) {
-        State nextState = new State(state);
-        nextState.position = new Vector(position).add(direction);
-        nextState.path.add(nextState.position);
-        queue.add(nextState);
-      }
-    }
-  }
-
-  private static void attack(Unit unit, TreeMap<Vector, Unit> units) {
-    Unit unitInRange = getUnitInRange(unit, units);
-    if (unitInRange != null) {
-      // System.out.println(String.format("Attacking %s with unit at %s", unitInRange.position, unit.position));
-      unitInRange.hp -= Unit.ATTACK_POINTS;
-      if (unitInRange.hp <= 0) {
-        // System.out.println("They died!");
-        units.remove(unitInRange.position);
-      }
-    }
-  }
-
-  private static OptionalInt getResult(TreeMap<Vector, Unit> units) {
-    boolean onlyElves = true;
-    boolean onlyGoblins = true;
-    int totalHps = 0;
-    for (Unit unit : units.values()) {
-      // System.out.println(String.format("position: %s; HP: %d; isElf? %b", unit.position, unit.hp, unit.isElf));
-      onlyElves &= unit.isElf;
-      onlyGoblins &= !unit.isElf;
-      totalHps += unit.hp;
-    }
-    if (onlyElves || onlyGoblins) {
-      return OptionalInt.of(totalHps);
-    } else {
-      return OptionalInt.empty();
-    }
+    return new Battle(walls, units);
   }
 
   public static int getBattleOutcome(List<String> lines) {
-    Set<Vector> walls = parseWalls(lines);
-    TreeMap<Vector, Unit> units = parseUnits(lines);
-    int roundIndex = 0;
+    // System.out.println("getBattleOutcome");
+    Battle battle = parseWallsAndUnits(lines);
+    // System.out.println(battle);
+    // int nbElves = (int) battle.units.values().stream().filter(unit ->
+    // unit.isElf).count();
+    // int nbGoblins = (int) battle.units.values().stream().filter(unit ->
+    // !unit.isElf).count();
+    // System.out.println(String.format("Number of elves: %d; number of goblins:
+    // %d", nbElves, nbGoblins));
+    int roundCount = 0;
     while (true) {
-      // System.out.println(String.format("Round %d", roundIndex));
-      for (Unit unit : units.values().stream().collect(Collectors.toList())) {
-        OptionalInt result = getResult(units);
-        if (result.isPresent()) {
-          // System.out.println(String.format("Battle ended, last completed round: %d, total remaining HP: %d", roundIndex, result.getAsInt()));
-          return result.getAsInt() * roundIndex;
-        }
-        moveUnit(unit, units, walls);
-        attack(unit, units);
+      boolean targetsExist = battle.playTurn();
+      // System.out.println(battle);
+      // battle.playTurn();
+      // nbElves = (int) battle.units.values().stream().filter(unit ->
+      // unit.isElf).count();
+      // nbGoblins = (int) battle.units.values().stream().filter(unit ->
+      // !unit.isElf).count();
+      // System.out.println(String.format("Number of elves: %d; number of goblins:
+      // %d", nbElves, nbGoblins));
+      if (!targetsExist) {
+        break;
       }
-      roundIndex++;
+      roundCount++;
     }
+    return roundCount * battle.outcome();
   }
 }
--- src/test/java/com/pjestin/aoc2018/Day15Test.java
+++ src/test/java/com/pjestin/aoc2018/Day15Test.java
@@ -9,27 +9,35 @@ import java.util.List;
 import com.pjestin.lib.FileUtils;
 
 public class Day15Test {
-  private static List<String> inputTestLines, inputTest2Lines, inputTest3Lines, inputTest4Lines, inputTest5Lines, inputTest6Lines, inputLines;
+  private static List<String> inputTestLines, inputTest2Lines, inputTest3Lines, inputTest4Lines, inputTest5Lines,
+      inputTest6Lines, inputLines;
 
   @BeforeAll
   public static void setUp() throws FileNotFoundException {
-    inputTestLines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test.txt"));
-    inputTest2Lines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-2.txt"));
-    inputTest3Lines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-3.txt"));
-    inputTest4Lines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-4.txt"));
-    inputTest5Lines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-5.txt"));
-    inputTest6Lines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-6.txt"));
-    inputLines = FileUtils.readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input.txt"));
+    inputTestLines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test.txt"));
+    inputTest2Lines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-2.txt"));
+    inputTest3Lines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-3.txt"));
+    inputTest4Lines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-4.txt"));
+    inputTest5Lines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-5.txt"));
+    inputTest6Lines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input-test-6.txt"));
+    inputLines = FileUtils
+        .readLines(Paths.get("src", "test", "resources", "com", "pjestin", "aoc2018", "day15", "input.txt"));
   }
 
   @Test
   public void getBattleOutcomeTest() {
     assertEquals(27730, Day15.getBattleOutcome(inputTestLines));
-    // assertEquals(36334, Day15.getBattleOutcome(inputTest2Lines));
-    // assertEquals(39514, Day15.getBattleOutcome(inputTest3Lines));
-    // assertEquals(27755, Day15.getBattleOutcome(inputTest4Lines));
-    // assertEquals(28944, Day15.getBattleOutcome(inputTest5Lines));
-    // assertEquals(18740, Day15.getBattleOutcome(inputTest6Lines));
+    assertEquals(36334, Day15.getBattleOutcome(inputTest2Lines));
+    assertEquals(39514, Day15.getBattleOutcome(inputTest3Lines));
+    assertEquals(27755, Day15.getBattleOutcome(inputTest4Lines));
+    assertEquals(28944, Day15.getBattleOutcome(inputTest5Lines));
+    assertEquals(18740, Day15.getBattleOutcome(inputTest6Lines));
     // assertEquals(1, Day15.getBattleOutcome(inputLines));
   }
 }
@@ -39,3 +47,4 @@ public class Day15Test {
 // 262400 too high
 // 260073 incorrect
 // 260370 incorrect
+// 194950 incorrect
