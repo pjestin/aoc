55c32fb004e942c3c9115a66606431fdce720fb3
Pierre Jestin <pierre.jestin@gmail.com>
2021-07-17 10:24:54 +0200

    Day 17 part 2

--- day17/day17.py
+++ day17/day17.py
@@ -1,4 +1,5 @@
 NUMBER_OF_INSERTIONS = 2017
+NUMBER_OF_ANGRY_INSERTIONS = 50_000_000
 
 
 def find_insertion_result(steps: int) -> int:
@@ -9,3 +10,13 @@ def find_insertion_result(steps: int) -> int:
         lst = lst[: position + 1] + [value] + lst[position + 1 :]
         position += 1
     return lst[position + 1]
+
+
+def find_angry_insertion_result(steps: int) -> int:
+    index = 0
+    second_value = 0
+    for value in range(1, NUMBER_OF_ANGRY_INSERTIONS + 1):
+        index = 1 + (index + steps) % value
+        if index == 1:
+            second_value = value
+    return second_value
