ae9d859b4282a6a361720d2265833ae1c105472c
Pierre Jestin <pierre.jestin@gmail.com>
2021-07-18 17:42:37 +0200

    Day 21 part 1

---
+++ day21/test_day21.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from day21.day21 import count_pixels_after_transformations
+
+
+class Day21Test(TestCase):
+    def setUp(self) -> None:
+        with open("day21/input.txt", "r") as file:
+            self.inputFile = file.read().splitlines()
+        with open("day21/input-test.txt", "r") as file:
+            self.inputTestFile = file.read().splitlines()
+
+    def test_count_pixels_after_transformations(self) -> None:
+        self.assertEqual(12, count_pixels_after_transformations(self.inputTestFile, 2))
+        self.assertEqual(117, count_pixels_after_transformations(self.inputFile, 5))
