d2f199da46cf4b3d80753e1e10921871077475ed
Pierre Jestin <pierre.jestin@gmail.com>
2022-04-17 11:56:30 +0200

    Day 24 part 1

--- src/day24.rs
+++ src/day24.rs
@@ -9,7 +9,7 @@ fn parse_weights(lines: Lines<BufReader<File>>) -> Vec<usize> {
     .collect()
 }
 
-fn find_ways_to_fit(
+fn find_fitting_groups(
   weights: &[usize],
   group_weight: usize,
   current_weights: &Vec<usize>,
@@ -19,80 +19,56 @@ fn find_ways_to_fit(
   } else if weights.len() == 0 {
     return vec![];
   }
-  let mut ways: Vec<HashSet<usize>> = vec![];
+  let mut groups: Vec<HashSet<usize>> = vec![];
   if weights[0] <= group_weight {
     let mut weights_with_first = current_weights.clone();
     weights_with_first.push(weights[0]);
-    let ways_with_first = find_ways_to_fit(
+    let groups_with_first = find_fitting_groups(
       &weights[1..],
       group_weight - weights[0],
       &weights_with_first,
     );
-    ways.extend(ways_with_first);
+    groups.extend(groups_with_first);
   }
-  let ways_without_first = find_ways_to_fit(&weights[1..], group_weight, &current_weights.clone());
-  ways.extend(ways_without_first);
-  return ways;
+  let groups_without_first =
+    find_fitting_groups(&weights[1..], group_weight, &current_weights.clone());
+  groups.extend(groups_without_first);
+  return groups;
 }
 
-fn set_intersect(set1: &HashSet<usize>, set2: &HashSet<usize>) -> bool {
-  for e in set1 {
-    if set2.contains(e) {
-      return true;
+fn get_quantum_entanglement(group: &HashSet<usize>) -> Option<usize> {
+  let mut quantum_entanglement: usize = 1;
+  for &weight in group {
+    match quantum_entanglement.checked_mul(weight) {
+      None => return None,
+      Some(product) => quantum_entanglement = product,
     }
   }
-  for e in set2 {
-    if set1.contains(e) {
-      return true;
-    }
-  }
-  false
+  Some(quantum_entanglement)
 }
 
 pub fn find_best_quantum_entanglement(lines: Lines<BufReader<File>>) -> usize {
   let weights = parse_weights(lines);
   let group_weight: usize = weights.iter().sum::<usize>() / 3;
   let current_weights = vec![];
-  // println!("weights: {:?}", weights);
-  let ways: Vec<HashSet<usize>> =
-    find_ways_to_fit(weights.as_slice(), group_weight, &current_weights);
-  println!("Ways: {:?}", ways.len());
+
+  let groups: Vec<HashSet<usize>> =
+    find_fitting_groups(weights.as_slice(), group_weight, &current_weights);
+
   let mut min_weight_count = usize::MAX;
   let mut min_quantum_entanglement = usize::MAX;
-  for i in 0..ways.len() {
-    for j in (i + 1)..ways.len() {
-      for k in (j + 1)..ways.len() {
-        if set_intersect(&ways[i], &ways[j])
-          || set_intersect(&ways[i], &ways[k])
-          || set_intersect(&ways[j], &ways[k])
-        {
-          continue;
-        }
-        let quantum_entanglement_i = ways[i].iter().product::<usize>();
-        if ways[i].len() < min_weight_count
-          || (ways[i].len() == min_weight_count
-            && quantum_entanglement_i < min_quantum_entanglement)
-        {
-          min_weight_count = ways[i].len();
-          min_quantum_entanglement = quantum_entanglement_i;
-        }
-        let quantum_entanglement_j = ways[j].iter().product::<usize>();
-        if ways[j].len() < min_weight_count
-          || (ways[j].len() == min_weight_count
-            && quantum_entanglement_j < min_quantum_entanglement)
-        {
-          min_weight_count = ways[j].len();
-          min_quantum_entanglement = quantum_entanglement_j;
-        }
-        let quantum_entanglement_k = ways[k].iter().product::<usize>();
-        if ways[k].len() < min_weight_count
-          || (ways[k].len() == min_weight_count
-            && quantum_entanglement_k < min_quantum_entanglement)
-        {
-          min_weight_count = ways[k].len();
-          min_quantum_entanglement = quantum_entanglement_k;
-        }
-      }
+
+  for group in &groups {
+    let quantum_entanglement = get_quantum_entanglement(group);
+    if quantum_entanglement.is_none() {
+      continue;
+    }
+    if group.len() < min_weight_count
+      || (group.len() == min_weight_count
+        && quantum_entanglement.unwrap() < min_quantum_entanglement)
+    {
+      min_weight_count = group.len();
+      min_quantum_entanglement = quantum_entanglement.unwrap();
     }
   }
   min_quantum_entanglement
@@ -109,9 +85,9 @@ mod tests {
       99,
       find_best_quantum_entanglement(read_lines("./res/day24/input-test.txt").unwrap())
     );
-    // assert_eq!(
-    //   0,
-    //   find_best_quantum_entanglement(read_lines("./res/day24/input.txt").unwrap())
-    // );
+    assert_eq!(
+      10439961859,
+      find_best_quantum_entanglement(read_lines("./res/day24/input.txt").unwrap())
+    );
   }
 }
