a0e3a07b63f45690942aa8246beab82d636f6fee
Pierre Jestin <pierre.jestin@gmail.com>
2022-12-05 14:55:08 +0100

    2016 Day 19 part 2

--- 2016/src/day19.rs
+++ 2016/src/day19.rs
@@ -1,28 +1,48 @@
-pub fn find_elf_with_presents(n_elves: usize) -> usize {
-  let mut presents: Vec<usize> = vec![1; n_elves];
-  let mut next_elves: Vec<usize> = { 0..n_elves }.map(|index| (index + 1) % n_elves).collect();
-  let mut remaining_elves: usize = n_elves;
-  let mut elf_index: usize = 0;
+use std::collections::VecDeque;
 
-  loop {
-    if remaining_elves == 1 {
-      return elf_index + 1;
-    }
-    let next_elf_index: usize = next_elves[elf_index];
-    presents[elf_index] += presents[next_elf_index];
-    next_elves[elf_index] = next_elves[next_elf_index];
-    remaining_elves -= 1;
-    elf_index = next_elves[elf_index];
+pub fn find_elf_with_presents(n_elves: usize) -> usize {
+  let mut elves: VecDeque<usize> = VecDeque::from_iter(1..=n_elves);
+
+  while elves.len() > 1 {
+    let first: usize = elves.pop_front().unwrap();
+    let _second: usize = elves.pop_front().unwrap();
+    elves.push_back(first);
   }
+
+  *elves.front().unwrap()
+}
+
+pub fn find_elf_with_presents_across(n_elves: usize) -> usize {
+  let mut queue_a: VecDeque<usize> = VecDeque::from_iter(1..=(n_elves / 2));
+  let mut queue_b: VecDeque<usize> = VecDeque::from_iter((n_elves / 2 + 1)..=n_elves);
+
+  while queue_a.len() >= 1 {
+    let first: usize = queue_a.pop_front().unwrap();
+    let _second: usize = queue_b.pop_front().unwrap();
+    queue_b.push_back(first);
+
+    if queue_b.len() - queue_a.len() >= 2 {
+      let balanced_elf: usize = queue_b.pop_front().unwrap();
+      queue_a.push_back(balanced_elf);
+    }
+  }
+
+  *queue_b.front().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::day19::find_elf_with_presents;
+  use crate::day19::{find_elf_with_presents, find_elf_with_presents_across};
 
   #[test]
   fn test_find_elf_with_presents() {
     assert_eq!(3, find_elf_with_presents(5));
     assert_eq!(1815603, find_elf_with_presents(3004953));
+  }
+
+  #[test]
+  fn test_find_elf_with_presents_across() {
+    assert_eq!(2, find_elf_with_presents_across(5));
+    assert_eq!(1410630, find_elf_with_presents_across(3004953));
   }
 }
