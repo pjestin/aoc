3c4f639d6eba1594633362ddcf7d583ab0c54b35
Pierre Jestin <pierre.jestin@gmail.com>
2022-12-11 17:33:47 +0100

    2016 Day 22 part 2 - result by display

--- 2016/src/day22.rs
+++ 2016/src/day22.rs
@@ -1,10 +1,10 @@
-// use crate::vector::Vector;
+use crate::vector::Vector;
 use regex::{Captures, Regex};
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
 struct Node {
-  // position: Vector,
+  position: Vector,
   // size: i64,
   used: i64,
   avail: i64,
@@ -23,10 +23,10 @@ fn parse_nodes(lines: Lines<BufReader<File>>) -> Vec<Node> {
     if re.is_match(&unwrapped_line) {
       let captures: Captures = re.captures(&unwrapped_line).unwrap();
       nodes.push(Node {
-        // position: Vector {
-        //   x: captures[1].parse::<i64>().unwrap(),
-        //   y: captures[2].parse::<i64>().unwrap(),
-        // },
+        position: Vector {
+          x: captures[1].parse::<i64>().unwrap(),
+          y: captures[2].parse::<i64>().unwrap(),
+        },
         // size: captures[3].parse::<i64>().unwrap(),
         used: captures[4].parse::<i64>().unwrap(),
         avail: captures[5].parse::<i64>().unwrap(),
@@ -63,16 +63,73 @@ pub fn count_viable_pairs(lines: Lines<BufReader<File>>) -> usize {
   viable_pair_count
 }
 
+fn display_nodes(nodes: &Vec<Node>) -> String {
+  let max_x: usize = nodes
+    .iter()
+    .map(|node| node.position.x as usize)
+    .max()
+    .unwrap();
+  let max_y: usize = nodes
+    .iter()
+    .map(|node| node.position.y as usize)
+    .max()
+    .unwrap();
+
+  let mut node_strings: Vec<Vec<String>> = vec![vec![String::new(); max_x + 1]; max_y + 1];
+  // println!("node_strings: {:?}", node_strings);
+
+  for node in nodes {
+    let row: usize = node.position.y as usize;
+    let col: usize = node.position.x as usize;
+
+    if node.position.x == 0 && node.position.y == 0 {
+      node_strings[row][col] = "(.)".to_owned();
+    } else if node.position.x as usize == max_x && node.position.y == 0 {
+      node_strings[row][col] = " G ".to_owned();
+    } else if node.used == 0 {
+      node_strings[row][col] = " _ ".to_owned();
+    } else if node.used > 100 {
+      node_strings[row][col] = " # ".to_owned();
+    } else {
+      node_strings[row][col] = " . ".to_owned();
+    }
+  }
+
+  node_strings
+    .iter()
+    .map(|row| row.join(""))
+    .collect::<Vec<String>>()
+    .join("\n")
+}
+
+pub fn count_steps_to_goal_data(lines: Lines<BufReader<File>>) -> usize {
+  let nodes: Vec<Node> = parse_nodes(lines);
+  println!("{}", display_nodes(&nodes));
+  0
+}
+
 #[cfg(test)]
 mod tests {
-  use crate::day22::count_viable_pairs;
+  use crate::day22::{count_steps_to_goal_data, count_viable_pairs};
   use crate::file_utils::read_lines;
 
   #[test]
-  fn test_scramble_password() {
+  fn test_count_viable_pairs() {
     assert_eq!(
       934,
       count_viable_pairs(read_lines("./res/day22/input.txt").unwrap())
+    );
+  }
+
+  #[test]
+  fn test_count_steps_to_goal_data() {
+    assert_eq!(
+      7,
+      count_steps_to_goal_data(read_lines("./res/day22/input-test.txt").unwrap())
+    );
+    assert_eq!(
+      207,
+      count_steps_to_goal_data(read_lines("./res/day22/input.txt").unwrap())
     );
   }
 }
