257baaadd739e45f13400a45b666d1a5b9e52885
Pierre Jestin <pierre.jestin@gmail.com>
2022-12-12 14:31:42 +0100

    2016 Day 23 part 2

--- 2016/src/day23.rs
+++ 2016/src/day23.rs
@@ -9,9 +9,13 @@ pub fn run_program(lines: Lines<BufReader<File>>) -> i64 {
   assembunny.registers["a"]
 }
 
+pub fn run_translated_program(input: i64) -> i64 {
+  { 1..=input }.reduce(|acc, x| acc * x).unwrap() + 5400
+}
+
 #[cfg(test)]
 mod tests {
-  use crate::day23::run_program;
+  use crate::day23::{run_program, run_translated_program};
   use crate::file_utils::read_lines;
 
   #[test]
@@ -24,5 +28,11 @@ mod tests {
       10440,
       run_program(read_lines("./res/day23/input.txt").unwrap())
     );
+  }
+
+  #[test]
+  fn test_run_translated_program() {
+    assert_eq!(10440, run_translated_program(7));
+    assert_eq!(479007000, run_translated_program(12));
   }
 }
