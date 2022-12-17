9fee77bf110b1eb26ef70eaccea485d52d95925c
Pierre Jestin <pierre.jestin@gmail.com>
2022-12-17 12:18:10 +0100

    2022 Day 16 part 2

--- 2022/src/day16/index.ts
+++ 2022/src/day16/index.ts
@@ -24,24 +24,22 @@ class Valve {
 class State {
   time: number;
   valve: string;
-  elephantValve: string;
   rate: number;
   pressure: number;
   openValves: string[];
-  elephantOpenValves: string[];
+  combination: string;
 
-  constructor(time: number, valve: string, elephantValve: string, rate: number, pressure: number, openValves: string[], elephantOpenValves: string[]) {
+  constructor(time: number, valve: string, rate: number, pressure: number, openValves: string[], combination: string) {
     this.time = time;
     this.valve = valve;
-    this.elephantValve = elephantValve;
     this.rate = rate;
     this.pressure = pressure;
     this.openValves = openValves;
-    this.elephantOpenValves = elephantOpenValves;
+    this.combination = combination;
   }
 
   toString() {
-    return `${this.valve};${this.openValves};${this.elephantValve};${this.elephantOpenValves}`;
+    return `${this.valve};${this.openValves}`;
   }
 }
 
@@ -60,34 +58,23 @@ function parseGraph(input: string[]): { [name: string]: Valve } {
   return graph;
 }
 
-function notEnoughPressure(graph: { [name: string]: Valve }, state: State, maxPressure: number): boolean {
-  const remainingRate: number = Object.keys(graph)
-    .filter(valve => !state.openValves.includes(valve) && !state.elephantOpenValves.includes(valve))
-    .map(closedValve => graph[closedValve].rate)
-    .reduce((acc, rate) => acc + rate, 0);
-  const rate: number = state.rate + remainingRate;
-  const pressure: number = rate * (MAX_TIME - state.time) + state.pressure;
-  return pressure <= maxPressure
-}
-
-export function findMostPressure(input: string[]): number {
-  const graph: { [name: string]: Valve } = parseGraph(input);
+function findMostPressurePerCombination(graph: { [name: string]: Valve }, maxTime: number): { [combination: string]: number } {
   const nOpenableValves: number = Object.values(graph).filter(valve => valve.rate > 0).length;
+  let combinations: { [combination: string]: number } = {};
 
-  let maxPressure: number = 0;
   let visited: Set<string> = new Set;
   let queue: Queue<State> = new Queue(100000000);
-  queue.push(new State(0, FIRST_VALVE, FIRST_VALVE, 0, 0, [], []));
+  queue.push(new State(0, FIRST_VALVE, 0, 0, [], ''));
 
   while (!queue.isEmpty()) {
     const state: State = queue.pop();
 
-    maxPressure = Math.max(maxPressure, state.pressure + (MAX_TIME - state.time) * state.rate);
+    combinations[state.combination] = Math.max(combinations[state.combination] || 0, state.pressure + (maxTime - state.time) * state.rate);
 
-    if (state.time === MAX_TIME
+    if (state.time === maxTime
       || state.openValves.length >= nOpenableValves
       || visited.has(state.toString())
-      || notEnoughPressure(graph, state, maxPressure)) {
+    ) {
       continue;
     }
 
@@ -97,11 +84,10 @@ export function findMostPressure(input: string[]): number {
       queue.push(new State(
         state.time + 1,
         state.valve,
-        state.valve,
         state.rate + graph[state.valve].rate,
         state.pressure + state.rate,
         [...state.openValves, state.valve],
-        state.elephantOpenValves,
+        [...state.openValves, state.valve].sort().join(';'),
       ));
     }
 
@@ -109,99 +95,46 @@ export function findMostPressure(input: string[]): number {
       queue.push(new State(
         state.time + 1,
         destination,
-        state.elephantValve,
         state.rate,
         state.pressure + state.rate,
         state.openValves,
-        state.elephantOpenValves,
+        state.combination,
       ));
     }
   }
 
-  return maxPressure;
+  return combinations;
+}
+
+export function findMostPressure(input: string[]): number {
+  const graph: { [name: string]: Valve } = parseGraph(input);
+
+  const combinations: { [combination: string]: number } = findMostPressurePerCombination(graph, MAX_TIME);
+
+  return Math.max(...Object.values(combinations));
+}
+
+function areCombinationsCompatible(combination1: string, combination2: string): boolean {
+  for (const valve of combination1.split(';')) {
+    if (combination2.includes(valve)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export function findMostPressureWithElephant(input: string[]): number {
   const graph: { [name: string]: Valve } = parseGraph(input);
-  const nOpenableValves: number = Object.values(graph).filter(valve => valve.rate > 0).length;
-
+  const combinations: { [combination: string]: number } = findMostPressurePerCombination(graph, MAX_TIME_WITH_ELEPHANT);
+  const combinationKeys: string[] = Object.keys(combinations);
   let maxPressure: number = 0;
-  let visited: Set<string> = new Set;
-  let queue: Queue<State> = new Queue(100000000);
-  queue.push(new State(0, FIRST_VALVE, FIRST_VALVE, 0, 0, [], []));
 
-  while (!queue.isEmpty()) {
-    const state: State = queue.pop();
-
-    // maxPressure = Math.max(maxPressure, state.pressure + (MAX_TIME_WITH_ELEPHANT - state.time) * state.rate);
-    if (state.pressure + (MAX_TIME_WITH_ELEPHANT - state.time) * state.rate > maxPressure) {
-      maxPressure = state.pressure + (MAX_TIME_WITH_ELEPHANT - state.time) * state.rate;
-      console.log('maxPressure:', maxPressure, '; state:', state)
-    }
-
-    if (state.time === MAX_TIME_WITH_ELEPHANT
-      || state.openValves.length + state.elephantOpenValves.length >= nOpenableValves
-      || visited.has(state.toString())
-      || notEnoughPressure(graph, state, maxPressure)) {
-      continue;
-    }
-
-    visited.add(state.toString());
-
-    if (state.valve !== state.elephantValve
-      && !state.openValves.includes(state.valve) && !state.openValves.includes(state.elephantValve)
-      && !state.elephantOpenValves.includes(state.valve) && !state.elephantOpenValves.includes(state.elephantValve)
-      && graph[state.valve].rate > 0 && graph[state.elephantValve].rate > 0) {
-      queue.push(new State(
-        state.time + 1,
-        state.valve,
-        state.elephantValve,
-        state.rate + graph[state.valve].rate + graph[state.elephantValve].rate,
-        state.pressure + state.rate,
-        [...state.openValves, state.valve],
-        [...state.elephantOpenValves, state.elephantValve],
-      ));
-    }
-
-    if (!state.openValves.includes(state.valve) && !state.elephantOpenValves.includes(state.valve) && graph[state.valve].rate > 0) {
-      for (const elephantDestination of graph[state.elephantValve].destinations) {
-        queue.push(new State(
-          state.time + 1,
-          state.valve,
-          elephantDestination,
-          state.rate + graph[state.valve].rate,
-          state.pressure + state.rate,
-          [...state.openValves, state.valve],
-          state.elephantOpenValves,
-        ));
-      }
-    }
-
-    if (!state.openValves.includes(state.elephantValve) && !state.elephantOpenValves.includes(state.elephantValve) && graph[state.elephantValve].rate > 0) {
-      for (const destination of graph[state.valve].destinations) {
-        queue.push(new State(
-          state.time + 1,
-          destination,
-          state.elephantValve,
-          state.rate + graph[state.elephantValve].rate,
-          state.pressure + state.rate,
-          state.openValves,
-          [...state.elephantOpenValves, state.elephantValve],
-        ));
-      }
-    }
-
-    for (const destination of graph[state.valve].destinations) {
-      for (const elephantDestination of graph[state.elephantValve].destinations) {
-        queue.push(new State(
-          state.time + 1,
-          destination,
-          elephantDestination,
-          state.rate,
-          state.pressure + state.rate,
-          state.openValves,
-          state.elephantOpenValves,
-        ));
+  for (let i = 0; i < combinationKeys.length; i++) {
+    for (let j = i + 1; j < combinationKeys.length; j++) {
+      const combination1: string = combinationKeys[i];
+      const combination2: string = combinationKeys[j];
+      if (areCombinationsCompatible(combination1, combination2)) {
+        maxPressure = Math.max(maxPressure, combinations[combination1] + combinations[combination2]);
       }
     }
   }
--- 2022/src/day16/run-test.ts
+++
@@ -1,6 +0,0 @@
-import { findMostPressureWithElephant } from ".";
-import * as fs from 'fs';
-import * as path from 'path';
-
-let input: string[] = fs.readFileSync(path.resolve('/home/pierre/src/aoc/2022/src/day16/input.txt'), 'utf8').split('\n').slice(0, -1);
-console.log(findMostPressureWithElephant(input));
